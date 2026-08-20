1edd81e9f22e7559ed83f94fa5a32b83814376eb
Tejas Gopale <tejasgopale111@gmail.com>
2026-08-20 16:43:52 +0530

    cookies

---
+++ src/main/java/com/tejas/AddServlet.java
@@ -0,0 +1,69 @@
+package com.tejas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.catalina.connector.Response;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AddServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		int i =Integer.parseInt(req.getParameter("num1"));
+		
+		int j = Integer.parseInt(req.getParameter("num2"));
+	
+			int sum = i + j ;
+			
+		//cookie 
+			Cookie cookie = new Cookie("sum",sum + "");
+			res.addCookie(cookie);
+			res.sendRedirect("sq");
+			
+			
+			//Session 	
+//			System.out.println("result of 2 sum is : " + sum);
+//		
+//			HttpSession session = req.getSession();
+//			session.setAttribute("sum", sum);
+//			
+//			res.sendRedirect("sq");
+	
+			/* Request Despatcher
+			 * req.setAttribute("sum", sum);
+				RequestDispatcher rd = req.getRequestDispatcher("sq");  // pass this into the web.xml file tooo
+				rd.forward(req, res);
+			 */
+			
+/* 
+ * For Normal Prinitng			
+ *
+ *		PrintWriter out =	res.getWriter();
+ *		out.println("Result is "+sum);
+*/
+	}
+	
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int i =Integer.parseInt(req.getParameter("num1"));
+		
+		int j = Integer.parseInt(req.getParameter("num2"));
+	
+			int sum = i + j ;
+			
+			System.out.println("result of 2 sum is : " + sum);
+			
+		PrintWriter out =	res.getWriter();
+		out.println("Result is "+sum);
+	}
+
+}
---
+++ src/main/java/com/tejas/SquareServlet.java
@@ -0,0 +1,36 @@
+package com.tejas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SquareServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException {
+//		Session 
+//		HttpSession session = req.getSession();
+//		int k = (int) session.getAttribute("sum");
+		int k = 0;
+	 Cookie cookie [] =	req.getCookies();
+		 for(Cookie c : cookie) {
+			 if(c.getName().equals("sum")) {
+				 k=Integer.parseInt(c.getValue());
+			 }
+		 }
+		k = k*k;
+		
+		PrintWriter out =	res.getWriter();
+		out.println("Result is "+k);
+		
+		
+		//int k = (int) req.getAttribute("sum");
+//		int k =Integer.parseInt(req.getParameter("sum"));	
+//		PrintWriter out =	res.getWriter();
+//		out.println("Result is "+k);
+	}
+}
